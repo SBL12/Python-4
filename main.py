339457aa6b94f36fde8e2b51d889f8656c17a295
Suyash L <suyashblal@gmail.com>
2021-03-22 22:32:23 +0000

    Day, Family, Number lists

---
+++ main.py
@@ -0,0 +1,12 @@
+my_intlist = [1, 2, 3, 4, 5]
+my_charlist2 = ['Mother', 'Father', 'Suyash', 'Sanjna']
+my_daylist = ['Monday', 'Tudesday', 'Wednesday', 'Thursday', 'Friday']
+
+for e in my_intlist:
+  print(e)
+
+for name in my_charlist2:
+  print(name)
+
+  for day in my_daylist:
+    print (day)
